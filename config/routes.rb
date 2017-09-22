6c88794c9ba478d8cdeeea9412318b4d0000a8dc
Aniqa Elahi <aniqaelahi@yahoo.com>
2017-09-22 20:52:33 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_for :users, controllers: { omniauth_callbacks: 'auth/callbacks' }
+  get '/', to: 'welcome#home'
+  # get '/auth/facebook/callback' => 'sessions#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
